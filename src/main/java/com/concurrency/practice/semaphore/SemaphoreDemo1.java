58fa74f7f57af546572d43b20885d52e1a8c1654
chenchen <chenchen@bitnei.cn>
2020-07-09 20:44:00 +0800

    -[modify]Semaphore实践，使用semaphore对访问慢服务的线程数量进行限制，降低慢服务的并发负荷；

--- src/main/java/com/concurrency/practice/semaphore/SemaphoreDemo1.java
+++ src/main/java/com/concurrency/practice/semaphore/SemaphoreDemo1.java
@@ -2,6 +2,7 @@ package com.concurrency.practice.semaphore;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
 
 /**
  * @author altenchen
@@ -9,6 +10,8 @@ import java.util.concurrent.Executors;
  * @description 功能
  */
 public class SemaphoreDemo1 {
+    
+    private static Semaphore semaphore = new Semaphore(3, false);
     
     public static void main(String[] args) {
         ExecutorService service = Executors.newFixedThreadPool(50);
@@ -22,12 +25,19 @@ public class SemaphoreDemo1 {
     
         @Override
         public void run() {
-            System.out.println("调用了慢服务：" + Thread.currentThread().getName());
             try {
-                Thread.sleep(3000);
+                semaphore.acquire(3);
+                System.out.println(Thread.currentThread().getName() + "拿到了许可证，花费2秒执行慢服务");
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("慢服务执行完毕，" + Thread.currentThread().getName() + "释放了许可证");
+            semaphore.release(3);
         }
     }
     
