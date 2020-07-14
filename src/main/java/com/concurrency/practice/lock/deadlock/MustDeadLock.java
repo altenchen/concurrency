1a88e928447f3385738210eab406c6d291fe19ac
chenchen <altenchen@aliyun.com>
2020-07-14 23:02:57 +0800

    -[add]DeadLock，实现一个必然发生死锁的demo；

---
+++ src/main/java/com/concurrency/practice/lock/deadlock/MustDeadLock.java
@@ -0,0 +1,62 @@
+package com.concurrency.practice.lock.deadlock;
+
+/**
+ * @description:
+ * @create: 2020/7/14
+ * @author: altenchen
+ */
+public class MustDeadLock implements Runnable{
+
+    private int flag;
+
+    static Object o1 = new Object();
+
+    static Object o2 = new Object();
+
+    @Override
+    public void run() {
+        System.out.println("线程" + Thread.currentThread().getName() + "的flag为" + flag);
+        if (flag == 1) {
+            synchronized (o1) {
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                synchronized (o2) {
+                    System.out.println("线程1获得了两把锁");
+                }
+            }
+        }
+
+        if (flag == 2) {
+            synchronized (o2) {
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                synchronized (o1) {
+                    System.out.println("线程2获得了两把锁");
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        MustDeadLock r1 = new MustDeadLock();
+        MustDeadLock r2 = new MustDeadLock();
+
+        r1.flag = 1;
+        r2.flag = 2;
+
+        Thread t1 = new Thread(r1, "t1");
+        Thread t2 = new Thread(r2, "t2");
+
+        t1.start();
+        t2.start();
+
+    }
+}
